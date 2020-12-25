5a70eb65dc12c31cb3262ac94c6bff434506df85
vincent-peugnet <v.peugnet@free.fr>
2020-12-26 00:42:35 +0100

    stats on home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\SharableRepository;
+use App\Repository\UserRepository;
+use App\Repository\ValidationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,9 +14,18 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="app_homepage")
      */
-    public function index(): Response
+    public function index(SharableRepository $sharableRepository, UserRepository $userRepository, ValidationRepository $validationRepository): Response
     {
+        $userCount = $userRepository->count([]);
+        $sharableCount = $userRepository->count([]);
+        $validationCount = $validationRepository->count([]);
+        $userLimit = $this->getParameter('app.userLimit');
+
         return $this->render('home/index.html.twig', [
+            'userCount' => $userCount,
+            'sharableCount' => $sharableCount,
+            'validationCount' => $validationCount,
+            'userLimit' => $userLimit,
         ]);
     }
 }
