ef5ff2ce15a45dfa93e709db25d46a402dcc28a2
n-peugnet <n.peugnet@free.fr>
2021-01-14 19:40:08 +0100

    check for license header on each source file

--- src/Service/SchemeProcessor.php
+++ src/Service/SchemeProcessor.php
@@ -1,9 +1,39 @@
 <?php
 
+/**
+ * This file is part of Antilope
+ *
+ * Antilope is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * PHP version 7.2
+ *
+ * @package Antilope
+ * @author Nicolas Peugnet <n.peugnet@free.fr>
+ * @copyright 2020-2021 Nicolas Peugnet
+ * @license https://www.gnu.org/licenses/agpl-3.0.txt AGPL-3.0-or-later
+ */
+
 namespace App\Service;
 
 use Symfony\Component\DependencyInjection\EnvVarProcessorInterface;
 
+/**
+ * Environment variable processor to extract the scheme part of an url.
+ *
+ * This class is used in config files as an env var processor.
+ * Primarily to get the driver part of a db url in doctrine_migrations.yaml.
+ */
 class SchemeProcessor implements EnvVarProcessorInterface
 {
     public function getEnv(string $prefix, string $name, \Closure $getEnv)
