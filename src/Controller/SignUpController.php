9ba04992eec5bdac04efb8860bcdbb6b3ca2456e
vincent-peugnet <v.peugnet@free.fr>
2021-01-16 23:05:16 +0100

    email verify at sign up

--- src/Controller/SignUpController.php
+++ src/Controller/SignUpController.php
@@ -31,17 +31,27 @@ use App\Form\SignUpType;
 use App\Repository\InvitationRepository;
 use App\Repository\UserClassRepository;
 use App\Repository\UserRepository;
+use App\Security\EmailVerifier;
 use App\Security\LoginFormAuthenticator;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
 class SignUpController extends AbstractController
 {
     private $userLimitReached = false;
+    private $emailVerifier;
+
+    public function __construct(EmailVerifier $emailVerifier)
+    {
+        $this->emailVerifier = $emailVerifier;
+    }
 
     /**
      * @Route("/signup", name="sign_up")
@@ -87,12 +97,13 @@ class SignUpController extends AbstractController
                 )
             );
 
+            // stop user creation if no user class exist
+
             $userClass = $userClassRepository->findOneBy([], ['rank' => 'ASC']);
             $user->setUserClass($userClass);
 
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
-
 
             if ($needCode) {
                 $code = $form->get('code')->getData();
@@ -102,6 +113,17 @@ class SignUpController extends AbstractController
             }
 
             $entityManager->flush();
+
+            // generate a signed url and email it to the user
+            $this->emailVerifier->sendEmailConfirmation(
+                'app_verify_email',
+                $user,
+                (new TemplatedEmail())
+                    ->from(new Address('noreply@antilope.net', $this->getParameter('siteName')))
+                    ->to($user->getEmail())
+                    ->subject('Please Confirm your Email')
+                    ->htmlTemplate('sign_up/confirmation_email.html.twig')
+            );
 
             return $guardHandler->authenticateUserAndHandleSuccess(
                 $user,
@@ -116,5 +138,29 @@ class SignUpController extends AbstractController
             'form' => $form->createView(),
             'userLimitReached' => $this->userLimitReached,
         ]);
+    }
+
+
+
+    /**
+     * @Route("/verify/email", name="app_verify_email")
+     */
+    public function verifyUserEmail(Request $request): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        // validate email confirmation link, sets User::isVerified=true and persists
+        try {
+            $this->emailVerifier->handleEmailConfirmation($request, $this->getUser());
+        } catch (VerifyEmailExceptionInterface $exception) {
+            $this->addFlash('verify_email_error', $exception->getReason());
+
+            return $this->redirectToRoute('app_register');
+        }
+
+        // @TODO Change the redirect on success and handle or remove the flash message in your templates
+        // $this->addFlash('success', 'Your email address has been verified.');
+
+        return $this->redirectToRoute('app_homepage');
     }
 }
