fde9b1014ed7b7ebb0e7f0cc62c659a259006af1
vincent-peugnet <vincent-peugnet@riseup.net>
2021-02-03 23:22:50 +0100

    fix migration spell error  fix #58

--- migrations/mysql/Version20210128161954.php
+++ migrations/mysql/Version20210128161954.php
@@ -20,12 +20,12 @@ final class Version20210128161954 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user CHANGE last_activiy last_activity DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE user ADD last_activity DATETIME NOT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user CHANGE last_activity last_activiy DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE user DROP last_activiy');
     }
 }
