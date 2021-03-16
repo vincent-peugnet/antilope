bf77545e1dd038296d2f8e5b886a99038e8f44df
n-peugnet <n.peugnet@free.fr>
2021-03-16 19:49:14 +0100

    fix(bootstrap): image uploads now displays file name

---
+++ public/bootstrap-file.js
@@ -0,0 +1,4 @@
+$(document).on('change', '.custom-file-input', function () {
+    let fileName = $(this).val().replace(/\\/g, '/').replace(/.*\//, '');
+    $(this).parent('.custom-file').find('.custom-file-label').text(fileName).css('color', '#444');
+});
